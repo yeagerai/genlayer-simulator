b101df43ceddea5d3fb08e21c381c3789335de08
sevenearths <from_a_far@hushmail.com>
2024-04-04 09:39:15 +0100

    FIX: ethereum address are now hexadecimal

--- rpc/server.py
+++ rpc/server.py
@@ -33,11 +33,11 @@ def log_status(message):
     socketio.emit('status_update', {'message': message})
 
 def create_new_address() -> str:
-    new_address = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(40))
+    new_address = ''.join(random.choice(string.hexdigits) for _ in range(40))
     return '0x' + new_address
 
 def address_is_in_correct_format(address:str) -> bool:
-    pattern = r'^0x[A-Za-z0-9]{40}$'
+    pattern = r'^0x['+string.hexdigits+']{40}$'
     if re.fullmatch(pattern, address):
         return True
     return False
