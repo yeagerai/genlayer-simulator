67d79bc030619943242eb209bfea6a8ebccf78e7
Robert Johnstone <from_a_far@hushmail.com>
2024-04-24 22:21:04 +0100

    68 move test contracts (#71)
    * moved contracts to examples/contracts. deleted env check from debug_simulator.py. Pointed debug_simulator.py to the new home for contracts
    
    * example contracts are now avaliable inside the frontend container
    
    * feat: copy example files and seed local db
    
    * updated live path mappings for development and example contracts
    
    * fix: added optional dependencies for rollup on linux container
    
    * fix: removed examples volume
    
    ---------
    
    Co-authored-by: Bradley Suira <bradleysuira@gmail.com>

--- frontend/src/utils/db.ts
+++ frontend/src/utils/db.ts
@@ -1,6 +1,7 @@
 import type { ContractFile, DefaultContractState, DeployedContract } from '@/types'
 import Dexie, { type Table } from 'dexie'
-import { contract as WizardOfCoinContract } from '@/assets/contractExample'
+const contractsBlob = import.meta.glob('@/assets/examples/contracts/*.py', { as: 'raw' })
+import { v4 as uuidv4 } from 'uuid'
 
 export class GenLayerSimulatorDB extends Dexie {
   contractFiles!: Table<ContractFile>
@@ -21,10 +22,14 @@ export const db = new GenLayerSimulatorDB()
 
 export async function setupDB() {
   if ((await db.contractFiles.count()) === 0) {
-    db.contractFiles.add({
-      id: '1',
-      name: 'WizardOfCoin',
-      content: WizardOfCoinContract.content.trim()
-    })
+    for (const key of Object.keys(contractsBlob)) {
+      const raw = await contractsBlob[key]()
+      const name = key.split('/').pop()?.split('.')[0] || 'ExampleContract'
+      await db.contractFiles.add({
+        id: uuidv4(),
+        name,
+        content: raw?.trim()
+      })
+    }
   }
 }
