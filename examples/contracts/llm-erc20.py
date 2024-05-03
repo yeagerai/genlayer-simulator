fd97d8fc6dcec0b0a3bf6f7e5866fd8d487db674
Cristiam Da Silva <cristiam86@gmail.com>
2024-05-03 16:18:29 +0200

    fix: a few improvements

--- examples/contracts/llm-erc20.py
+++ examples/contracts/llm-erc20.py
@@ -2,23 +2,7 @@ import json
 from genvm.base.equivalence_principle import EquivalencePrinciple
 
 
-class WizzardOfCoin:
-    prompt_part_1: str = """You keep track of transactions between users 
-    and their balance in coins. 
-    The current balance for all users in JSON format is:"""
-
-    prompt_part_2: str = "The transaction to compute is:"
-
-    prompt_part_3: str = """Given the current balance in JSON format and 
-    the transaction provided, please provide the updated balances as a 
-    JSON object.
-    For every transaction, validate that the user sending the Coins has 
-    enough balance. If any transaction is invalid it shouldn't be processed. 
-    Update the balances based on the valid transactions only.
-    It is mandatory that you respond with only the JSON balances object, nothing 
-    else whatsoever. Don't include any other words or characters, your 
-    output must be only the balances JSON object"""
-
+class LlmErc20:
     def __init__(self, initial_owner: str, total_supply: int):
         super().__init__()
         self.balances = {}
@@ -26,11 +10,24 @@ class WizzardOfCoin:
 
     async def send(self, amount: int, from_address: str, to_address: str) -> None:
         prompt = f"""
-{self.prompt_part_1}
+You keep track of transactions between users and their balance in coins. 
+The current balance for all users in JSON format is:
 {json.dumps(self.balances)}
-{self.prompt_part_2}
+The transaction to compute is:
 {from_address} sends {amount} coins to {to_address}
-{self.prompt_part_3}"""
+For every transaction, validate that the user sending the Coins has 
+enough balance. If any transaction is invalid, it shouldn't be processed. 
+Update the balances based on the valid transactions only.
+Given the current balance in JSON format and the transaction provided, 
+please provide the result of your calculation with the following format:
+{{
+transaction_success: bool,          // Whether the transaction was successful
+transaction_error: str,             // Empty if transaction is successful
+updated_balances: object<str, int>  // Updated balances after the transaction
+}}
+It is mandatory that you respond with only the JSON output object, nothing 
+else whatsoever. Don't include any other words or characters, your 
+output must be only the JSON object"""
         final_result = {}
         async with EquivalencePrinciple(
             result=final_result,
@@ -42,10 +39,10 @@ class WizzardOfCoin:
         ) as eq:
             result = await eq.call_llm(prompt)
             result_clean = result.replace("True", "true").replace("False", "false")
-            result_json = json.loads(result_clean)
-            eq.set(result_json)
+            eq.set(result_clean)
 
-        self.balances = final_result["output"]
+        result_json = json.loads(final_result["output"])
+        self.balances = result_json["updated_balances"]
 
     
     def get_balances(self):
