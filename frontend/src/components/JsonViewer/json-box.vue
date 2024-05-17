b49fd1306247c1d016f1a4884ffb710cafef2623
Bradley Suira <bradleysuira@gmail.com>
2024-05-16 22:37:21 -0500

    feat: added json viewer

---
+++ frontend/src/components/JsonViewer/json-box.vue
@@ -0,0 +1,160 @@
+<script>
+import JsonString from "./types/json-string.vue";
+import JsonUndefined from "./types/json-undefined.vue";
+import JsonNumber from "./types/json-number.vue";
+import JsonBoolean from "./types/json-boolean.vue";
+import JsonObject from "./types/json-object.vue";
+import JsonArray from "./types/json-array.vue";
+import JsonFunction from "./types/json-function.vue";
+import JsonDate from "./types/json-date.vue";
+import JsonRegexp from "./types/json-regexp.vue";
+import { h } from "vue";
+export default {
+  name: "JsonBox",
+  inject: ["expandDepth", "keyClick"],
+  props: {
+    value: {
+      type: [Object, Array, String, Number, Boolean, Function, Date],
+      default: null,
+    },
+    keyName: {
+      type: String,
+      default: "",
+    },
+    sort: Boolean,
+    depth: {
+      type: Number,
+      default: 0,
+    },
+    previewMode: Boolean,
+  },
+  data() {
+    return {
+      expand: false,
+    };
+  },
+  mounted() {
+    // this.expand = this.previewMode || (this.depth >= this.expandDepth ? false : true);
+  },
+  methods: {
+    toggle() {
+      this.expand = !this.expand;
+
+      try {
+        this.$el.dispatchEvent(new Event("resized"));
+      } catch (e) {
+        // handle IE not supporting Event constructor
+        var evt = document.createEvent("Event");
+        evt.initEvent("resized", true, false);
+        this.$el.dispatchEvent(evt);
+      }
+    },
+  },
+  render() {
+    let elements = [];
+    let dataType;
+
+    if (this.value === null || this.value === undefined) {
+      dataType = JsonUndefined;
+    } else if (Array.isArray(this.value)) {
+      dataType = JsonArray;
+    } else if (Object.prototype.toString.call(this.value) === "[object Date]") {
+      dataType = JsonDate;
+    } else if (typeof this.value === "object") {
+      dataType = JsonObject;
+    } else if (typeof this.value === "number") {
+      dataType = JsonNumber;
+    } else if (typeof this.value === "string") {
+      dataType = JsonString;
+    } else if (typeof this.value === "boolean") {
+      dataType = JsonBoolean;
+    } else if (typeof this.value === "function") {
+      dataType = JsonFunction;
+    }
+    if (this.value.constructor === RegExp) {
+      // console.log("type", this.value.constructor === RegExp);
+      // this.value=this.value.toString()
+      dataType = JsonRegexp;
+    }
+    const complex =
+      this.keyName &&
+      this.value &&
+      (Array.isArray(this.value) ||
+        (typeof this.value === "object" &&
+          Object.prototype.toString.call(this.value) !== "[object Date]"));
+
+    if (!this.previewMode && complex) {
+      elements.push(
+        h("span", {
+          class: {
+            "jv-toggle": true,
+            open: !!this.expand,
+          },
+          onClick: this.toggle,
+        })
+      );
+    }
+
+    if (this.keyName) {
+      elements.push(
+        h("span", {
+          class: {
+            "jv-key": true,
+          },
+          onClick: () => {
+            this.keyClick(this.keyName);
+          },
+          innerText: `${this.keyName}:`,
+        })
+      );
+    }
+
+    elements.push(
+      h(dataType, {
+        class: {
+          "jv-push": true,
+        },
+        jsonValue: this.value,
+        keyName: this.keyName,
+        sort: this.sort,
+        depth: this.depth,
+        expand: this.expand,
+        previewMode: this.previewMode,
+        "onUpdate:expand": (value) => {
+          this.expand = value;
+        },
+      })
+    );
+
+    return h(
+      "div",
+      {
+        class: {
+          "jv-node": true,
+          "jv-key-node": Boolean(this.keyName) && !complex,
+          toggle: !this.previewMode && complex,
+        },
+      },
+      elements
+    );
+  },
+};
+</script>
+
+<style>
+.jv-node {
+  position: relative;
+}
+.jv-node:after {
+  content: ",";
+}
+.jv-node:last-of-type:after {
+  content: "";
+}
+.jv-node.toggle {
+  margin-left: 13px !important;
+}
+.jv-node .jv-node {
+  margin-left: 25px;
+}
+</style>
--- frontend/src/components/Simulator/NodeLogs.vue
+++ frontend/src/components/Simulator/NodeLogs.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue'
 import { webSocketClient } from '@/utils'
-import { useMainStore } from '@/stores';
+import { useMainStore, useUIStore } from '@/stores'
+import JsonViewer from '@/components/JsonViewer/json-viewer.vue'
+
 
 const mainStore = useMainStore()
+const uiStore = useUIStore()
 const scrollContainer = ref<HTMLDivElement>()
 const colorMap: Record<string, string> = {
   'info': 'text-blue-500',
@@ -12,21 +15,15 @@ const colorMap: Record<string, string> = {
   'success': 'text-green-500'
 }
 
-const getDataText = (data: any) => {
-  if (typeof data !== 'string') return JSON.stringify(data, null, 2)
-  return data
-}
-
-
 onMounted(() => {
   webSocketClient.on('status_update', (event) => {
     console.log('webSocketClient.details', event)
     mainStore.$patch((state) => {
-      state.nodeLogs.push({ date: new Date().toISOString(), message: event.message})
+      state.nodeLogs.push({ date: new Date().toISOString(), message: event.message })
     })
 
     scrollContainer.value?.scrollTo({ top: scrollContainer.value.scrollHeight, behavior: 'smooth' })
-    
+
   })
 })
 
@@ -40,9 +37,10 @@ onUnmounted(() => {
   <div class="z-20 flex flex-col h-full w-full absolute bottom-0 left-0">
     <div class="flex bg-slate-100 p-1 dark:bg-zinc-700 h-6">
     </div>
-    <div id="tutorial-node-output"  class="flex flex-col w-full overflow-y-auto h-full p-1 bg-white dark:bg-zinc-800 dark:text-white cursor-text">
-      <div v-show="mainStore.nodeLogs.length > 0"
-        class="flex flex-col overflow-y-auto scroll-smooth p-0" ref="scrollContainer">
+    <div id="tutorial-node-output"
+      class="flex flex-col w-full overflow-y-auto h-full p-1 bg-white dark:bg-zinc-800 dark:text-white cursor-text">
+      <div v-show="mainStore.nodeLogs.length > 0" class="flex flex-col overflow-y-auto scroll-smooth p-0"
+        ref="scrollContainer">
         <div v-for="({ message, date }, index) in mainStore.nodeLogs" :key="index" class="flex items-center">
           <div class="flex items-start" :class="colorMap[message?.response?.status] || 'text-black-500'">
 
@@ -51,7 +49,11 @@ onUnmounted(() => {
             <div v-if="typeof message === 'string'" class="flex text-xs ml-1 flex-1">"{{ message }}</div>
             <div v-else class="flex text-xs ml-1 flex-1">
               {{ message.function }} {{ message?.response?.message ? ` ===> ${message.response.message}` : '' }}
-              || {{ getDataText(message.response?.data || '') }}
+              <div class="flex text-xs ml-2" v-if="typeof message.response?.data === 'string'">
+                {{ message.response?.data || '' }}
+              </div>
+              <JsonViewer class="ml-2" :value="message.response?.data"
+                :theme="uiStore.mode === 'light' ? 'light' : 'dark'" :expand="false" sort />
             </div>
           </div>
         </div>
@@ -64,7 +66,11 @@ onUnmounted(() => {
   </div>
 </template>
 
-<style scoped>
+<style>
+.jv-code {
+  padding: 0 !important;
+}
+
 .logs-small-text {
   font-size: 0.5rem;
 }
