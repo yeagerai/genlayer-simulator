82464ae245dc8aa9e2a3a97384e48eb05078ea18
Bradley Suira <bradleysuira@gmail.com>
2024-05-20 07:05:57 -0500

    fix: removed non used comments

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -2,14 +2,14 @@
 declare module 'vue3-json-viewer' {
     import { AllowedComponentProps, App, Component, ComponentCustomProps, VNodeProps } from 'vue'
     interface JsonViewerProps {
-        value: Object | Array<any> | string | number | boolean; //对象
-        expanded: boolean; //是否自动展开
-        expandDepth: number; //展开层级
-        copyable: boolean | object; //是否可复制
-        sort: boolean;//是否排序
-        boxed: boolean;//是否boxed
-        theme: string;//主题 jv-dark | jv-light
-        previewMode: boolean;//是否可复制
+        value: Object | Array<any> | string | number | boolean; 
+        expanded: boolean; 
+        expandDepth: number; 
+        copyable: boolean | object; 
+        sort: boolean;
+        boxed: boolean;
+        theme: string;
+        previewMode: boolean;
         timeformat: (value: any) => string
     }
     type JsonViewerType = JsonViewerProps & VNodeProps & AllowedComponentProps & ComponentCustomProps
