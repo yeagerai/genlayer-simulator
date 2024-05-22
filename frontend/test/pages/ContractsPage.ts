20a0ab5eecb15d3fae55b829abd405804d63abec
Bradley Suira <bradleysuira@gmail.com>
2024-05-22 09:37:53 -0500

    fix: removed non used elements

--- frontend/test/pages/ContractsPage.ts
+++ frontend/test/pages/ContractsPage.ts
@@ -5,10 +5,6 @@ export class ContractsPage extends BasePage {
   override baseurl = 'http://localhost:8080/simulator/contracts'
   override visibleLocator: Locator = By.xpath("//h3[contains(text(), 'Your Contracts')]")
 
-  elements = {
-    contracts: ['storage', 'wizard_of_coin']
-  }
-
   async openContract(name: string) {
     const locator = By.xpath(`//div[contains(@class, 'truncate ... cursor-pointer') and contains(text(), '${name}')]`)
     return this.driver.wait(until.elementLocated(locator), 5000).click();
