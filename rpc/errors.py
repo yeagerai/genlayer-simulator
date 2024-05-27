07e4a0d1192a8763881731bf90ecc6d0eeb8e61b
Cristiam Da Silva <cristiam86@gmail.com>
2024-05-27 18:39:34 +0200

    feat: added item not found error

--- rpc/errors.py
+++ rpc/errors.py
@@ -9,3 +9,16 @@ class InvalidAddressError(Exception):
         self.address = address
         self.message = message
         super().__init__(self.message)
+
+
+class ItemNotFoundError(Exception):
+    """Exception raised when a given item is not found."""
+
+    def __init__(
+        self,
+        id,
+        message="Item not found.",
+    ):
+        self.id = id
+        self.message = message
+        super().__init__(self.message)
