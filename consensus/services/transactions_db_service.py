3ef395b789361b9ad04e875b7629d2941964513f
Cristiam Da Silva <cristiam86@gmail.com>
2024-05-27 07:24:13 +0200

    feat: added transaction db service

---
+++ consensus/services/transactions_db_service.py
@@ -0,0 +1,30 @@
+# consensus/services/transactions_db_service.py
+
+import json
+
+
+class TransactionsDBService:
+    def __init__(self, db_client):
+        self.db_client = db_client
+        self.db_state_table = "transactions"
+        self.db_audits_table = "transactions_audit"
+
+    def insert_transaction(
+        self, from_address: str, to_address: str, data: dict, value: float, type: int
+    ) -> None:
+        # Insert transaction into the transactions table
+        new_transaction = {
+            "from_address": from_address,
+            "to_address": to_address,
+            "data": json.dumps(data),
+            "value": value,
+            "type": type,
+        }
+        self.db_client.insert(self.db_state_table, new_transaction)
+
+        # Insert transaction audit record into the transactions_audit table
+        transaction_audit_record = {
+            transaction_id: "",
+            data: json.dumps(new_transaction),
+        }
+        self.db_client.insert(self.db_audits_table, transaction_audit_record)
