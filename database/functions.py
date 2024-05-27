1d724dbf6f7eafc61dd316639cf2019bb5ef6cd5
Cristiam Da Silva <cristiam86@gmail.com>
2024-05-27 18:45:19 +0200

    refactor: removed unused code

--- database/functions.py
+++ database/functions.py
@@ -1,5 +1,3 @@
-import json
-
 from database.credentials import get_genlayer_db_connection
 
 
@@ -23,52 +21,6 @@ class DatabaseFunctions:
             "config": validator[5],
             "updated_at": validator[6].strftime("%m/%d/%Y, %H:%M:%S"),
         }
-
-    def create_validator(
-        self, address: str, stake: int, provider: str, model: str, config: str
-    ):
-        self.cursor.execute(
-            "INSERT INTO current_state (id, data) VALUES (%s, %s);",
-            (address, json.dumps({"stake": stake})),
-        )
-        self.cursor.execute(
-            "INSERT INTO validators (address, stake, provider, model, config, created_at) VALUES (%s, %s, %s, %s, %s, CURRENT_TIMESTAMP);",
-            (address, stake, provider, model, config),
-        )
-        self.connection.commit()
-        return self.get_validator(address)
-
-    def delete_validator(self, address: str):
-        self.cursor.execute(
-            "DELETE FROM validators WHERE address = %s;",
-            (address,),
-        )
-        self.connection.commit()
-        return {}
-
-    def update_validator(
-        self,
-        validator_address: str,
-        stake: float,
-        provider: str,
-        model: str,
-        config: str,
-    ):
-        self.cursor.execute(
-            "UPDATE validators SET stake = %s, provider = %s, model = %s, config = %s, created_at = CURRENT_TIMESTAMP WHERE address = %s;",
-            (stake, provider, model, config, validator_address),
-        )
-        self.connection.commit()
-        return self.get_validator(validator_address)
-
-    def get_validator(self, address: str):
-        self.cursor.execute("SELECT * FROM validators WHERE address = %s", (address,))
-        self.connection.commit()
-        validator = self.cursor.fetchone()
-        if validator:
-            return self.validator_details(validator)
-        else:
-            return {}
 
     def all_validators(self):
         self.cursor.execute("SELECT * FROM validators")
