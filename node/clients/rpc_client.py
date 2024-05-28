cbe94f3593b11c1282a366d15f67271f7152f658
Cristiam Da Silva <cristiam86@gmail.com>
2024-05-28 16:56:45 +0200

    refactor: make rpc client generic

--- node/clients/rpc_client.py
+++ node/clients/rpc_client.py
@@ -1,6 +1,7 @@
 # consensus/clients/rpc_client.py
 
 import requests
+import uuid
 
 from node.errors import GenVMRPCErrorResponse
 
@@ -10,29 +11,16 @@ class RPCClient:
         self.rpc_url = rpc_url
         self.jsonrpc = "2.0"
 
-    def deploy_contract(
-        self,
-        from_address: str,
-        contract_code: str,
-        constructor_args: str,
-        class_name: str,
-        leader_config: dict,
-    ) -> dict:
+    def call(self, method: str, params: list) -> dict:
         payload = {
             "jsonrpc": self.jsonrpc,
-            "method": "deploy_contract",
-            "params": [
-                from_address,
-                contract_code,
-                constructor_args,
-                class_name,
-                leader_config,
-            ],
-            "id": 3,
+            "method": method,
+            "params": params,
+            "id": str(uuid.uuid4()),
         }
         response = requests.post(self.rpc_url + "/api", json=payload).json()
 
         if response["result"]["status"] == "error":
             raise GenVMRPCErrorResponse(response["result"]["data"])
 
-        return response["result"]["data"]
+        return response["result"]
