8ca0bcd2c6460112c34369f161467ec8bc75e854
Bradley Suira <1889891+bradleySuira@users.noreply.github.com>
2024-06-04 08:50:04 -0500

    197 sim fe fix contract state and contract methods refresh after deploying and switching tabs (#199)
    * fix: fixed execute transactions abi update and selected method
    
    * fix: removed non used deps
    
    * fix: debounce only in contract content change
    
    * fix: fixed watchers for contract changes
    
    * fix: fixed watchers for contract changes
    
    * fix: call get constructor inputs in mounted for run and debug
    
    * docs: add required version and troubleshooting link (#198)
    
    * Update README.md add required version
    
    * Update README.md
    
    * docs: updated readme and contributing
    
    ---------
    
    Co-authored-by: Favour Kelvin <favourkelvin17@gmail.com>
    Co-authored-by: Cristiam Da Silva <cristiam86@gmail.com>

--- frontend/src/views/Simulator/RunDebugView.vue
+++ frontend/src/views/Simulator/RunDebugView.vue
@@ -20,7 +20,7 @@ const contract = computed(() =>
   store.contracts.find((contract) => contract.id === store.currentContractId)
 )
 const deployedContract = computed(() =>
-  store.deployedContracts.find((contract) => contract.contractId === store.currentContractId)
+  store.deployedContracts.find((c) => c.contractId === store.currentContractId)
 )
 const contractTransactions = computed(() => {
   if (deployedContract.value?.address && store.contractTransactions[deployedContract.value?.address]) {
@@ -185,7 +185,7 @@ const handleDeployContract = async ({
   }
 }
 
-const setDefaultState = async (contract: DeployedContract) => {
+const getContractAbi = async (contract: DeployedContract) => {
   try {
     // constructorInputs.value = JSON.parse(contract.defaultState || '{}')
     // TODO: check if we need to update again also we have an issue with conversion
@@ -237,24 +237,30 @@ const getConstructorInputs = async () => {
 
 const debouncedGetConstructorInputs = debounce(() => getConstructorInputs(), 3000)
 
-watch(() => deployedContract.value?.address, (newValue) => {
-  if (newValue && deployedContract.value) {
-    setDefaultState(deployedContract.value)
+watch(() => deployedContract.value?.contractId, (newValue) => {
+  if (newValue) {
+    getContractAbi(deployedContract.value!)
+  } 
+})
+
+
+watch(() => contract.value?.id, (newValue, oldValue) => {
+  if (newValue && newValue !== oldValue) {
+    getConstructorInputs()
   }
 })
 
-watch(() => contract.value, (newValue) => {
-  if (newValue && !loadingConstructorInputs.value) {
+watch(() => contract.value?.content, (newValue, oldValue) => {
+  if (newValue && newValue !== oldValue && !loadingConstructorInputs.value) {
     debouncedGetConstructorInputs()
   }
 })
 
 onMounted(() => {
   getConstructorInputs()
-
   if (deployedContract.value) {
-    setDefaultState(deployedContract.value)
-  }
+    getContractAbi(deployedContract.value)
+  } 
 })
 </script>
 
@@ -274,7 +280,7 @@ onMounted(() => {
         <ConstructorParameters :inputs="constructorInputs" :loading="loadingConstructorInputs"
           :error="errorConstructorInputs" @deploy-contract="handleDeployContract" :deploying="deployingContract" />
       </div>
-      <div class="flex flex-col" v-show="deployedContract">
+      <div class="flex flex-col" v-if="deployedContract">
         <div class="flex flex-col">
           <ContractState :abi="abi" :contract-state="contractState" :deployed-contract="deployedContract"
             :get-contract-state="handleGetContractState" :calling-state="callingContractState" />
