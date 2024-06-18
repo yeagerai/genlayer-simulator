c73c596cc53391052cdc3c8ec50574d90192acfc
Bradley Suira <bradleysuira@gmail.com>
2024-06-18 08:07:20 -0500

    feat: tutorial store first draft and refactor for tutorial container

---
+++ frontend/src/stores/tutorial.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+
+export const useTutorialStore = defineStore('tutorialStore', () => {
+  async function deployContract() {
+
+  }
+  async function callContractMethod() {
+
+  }
+  
+  return { deployContract, callContractMethod  }
+})
