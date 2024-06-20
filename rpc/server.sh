c40d1db477e10ba0047dcd4510aecb0d6fcdb14f
sevenearths <from_a_far@hushmail.com>
2024-06-20 09:59:07 +0100

    (snapshot)

--- rpc/server.sh
+++ rpc/server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "${VSCODEDEBUG}" = "true" ]; then
+        echo "!!! Starting server in DEBUG mode !!!"
         python -m debugpy --listen 0.0.0.0:${RPCDEBUGPORT} rpc/server.py
       else
+        echo "!!! Starting server in HOT RELOAD mode !!!"
         python rpc/server.py
       fi
