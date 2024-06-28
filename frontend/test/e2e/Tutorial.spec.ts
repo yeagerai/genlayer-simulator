2fb0eb279a96c6ee04f78c8a90df448c586ba518
Bradley Suira <bradleysuira@gmail.com>
2024-06-28 15:49:24 -0500

    feat: added deploying contract step test

--- frontend/test/e2e/Tutorial.spec.ts
+++ frontend/test/e2e/Tutorial.spec.ts
@@ -1,6 +1,5 @@
 import { WebDriver, By, until } from 'selenium-webdriver'
 
-
 import { ContractsPage } from '../pages/ContractsPage.js'
 import { before, describe, after, it } from 'node:test'
 import { expect } from 'chai'
@@ -21,17 +20,29 @@ describe('Tutorial - Run all tutorial steps', () => {
     await contractsPage.waitUntilVisible()
 
     const welcomeStepElement = await driver.wait(
-        until.elementLocated(By.xpath("//div[@data-testid='tutorial-step-#tutorial-welcome']"))
-      )
+      until.elementLocated(By.xpath("//div[@data-testid='tutorial-step-#tutorial-welcome']"))
+    )
     expect(welcomeStepElement, 'Welcome step should be visible').not.null
-    const headerElement = await welcomeStepElement.findElement(By.xpath("//div[contains(@class, 'v-step__header')]/div"))
+    const headerElement = await welcomeStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__header')]/div")
+    )
     const headerText = await headerElement.getText()
-    expect(headerText, 'Welcome step title should be visible').to.be.equal('Welcome to GenLayer Simulator!')
+    expect(headerText, 'Welcome step title should be visible').to.be.equal(
+      'Welcome to GenLayer Simulator!'
+    )
 
-    const contentElement = await welcomeStepElement.findElement(By.xpath("//div[contains(@class, 'v-step__content')]/div"))
+    const contentElement = await welcomeStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__content')]/div")
+    )
     const contentText = await contentElement.getText()
-    expect(contentText, 'Welcome step content should be visible').to.be.equal('This tutorial will guide you through the basics. Click “Next” to begin!')
-    const nextButton = await welcomeStepElement.findElement(By.xpath("//div[contains(@class, 'v-step__buttons')]/button[contains(@class, 'v-step__button-next')]"))
+    expect(contentText, 'Welcome step content should be visible').to.be.equal(
+      'This tutorial will guide you through the basics. Click “Next” to begin!'
+    )
+    const nextButton = await welcomeStepElement.findElement(
+      By.xpath(
+        "//div[contains(@class, 'v-step__buttons')]/button[contains(@class, 'v-step__button-next')]"
+      )
+    )
     expect(nextButton, 'Next button should be visible').not.null
     const nextButtonText = await nextButton.getText()
     expect(nextButtonText, 'Next button text should be equal to "Next"').to.be.equal('Next')
@@ -40,17 +51,60 @@ describe('Tutorial - Run all tutorial steps', () => {
 
   it('Should show the contract example step and navigate to the next', async () => {
     const contractExampleStepElement = await driver.wait(
-        until.elementLocated(By.xpath("//div[@data-testid='tutorial-step-#tutorial-contract-example']"))
+      until.elementLocated(
+        By.xpath("//div[@data-testid='tutorial-step-#tutorial-contract-example']")
       )
+    )
     expect(contractExampleStepElement, 'Contract example step should be visible').not.null
-    const headerElement = await contractExampleStepElement.findElement(By.xpath("//div[contains(@class, 'v-step__header')]/div"))
+    const headerElement = await contractExampleStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__header')]/div")
+    )
     const headerText = await headerElement.getText()
     expect(headerText, 'Contract example step title should be visible').to.be.equal('Code Editor')
 
-    const contentElement = await contractExampleStepElement.findElement(By.xpath("//div[contains(@class, 'v-step__content')]/div"))
+    const contentElement = await contractExampleStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__content')]/div")
+    )
     const contentText = await contentElement.getText()
-    expect(contentText, 'Contract example step content should be visible').to.be.equal("Write and edit your Intelligent Contracts here. The example contract 'ExampleContract.py' is preloaded for you.")
-    const nextButton = await contentElement.findElement(By.xpath("//div[contains(@class, 'v-step__buttons')]/button[contains(@class, 'v-step__button-next')]"))
+    expect(contentText, 'Contract example step content should be visible').to.be.equal(
+      "Write and edit your Intelligent Contracts here. The example contract 'ExampleContract.py' is preloaded for you."
+    )
+    const nextButton = await contentElement.findElement(
+      By.xpath(
+        "//div[contains(@class, 'v-step__buttons')]/button[contains(@class, 'v-step__button-next')]"
+      )
+    )
+    expect(nextButton, 'Next button should be visible').not.null
+    const nextButtonText = await nextButton.getText()
+    expect(nextButtonText, 'Next button text should be equal to "Next"').to.be.equal('Next')
+    await nextButton.click()
+  })
+
+  it('Should show the Run and Debug step and navigate to the next to Deploy', async () => {
+    const howToDeployStepElement = await driver.wait(
+      until.elementLocated(
+        By.xpath("//div[@data-testid='tutorial-step-#tutorial-how-to-deploy']")
+      )
+    )
+    expect(howToDeployStepElement, 'Deploying Contracts step should be visible').not.null
+    const headerElement = await howToDeployStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__header')]/div")
+    )
+    const headerText = await headerElement.getText()
+    expect(headerText, 'Deploying Contracts step title should be visible').to.be.equal('Deploying Contracts')
+
+    const contentElement = await howToDeployStepElement.findElement(
+      By.xpath("//div[contains(@class, 'v-step__content')]/div")
+    )
+    const contentText = await contentElement.getText()
+    expect(contentText, 'Deploying Contracts step content should be visible').to.be.equal(
+      "Click “Next” to automatically deploy your Intelligent Contract to the GenLayer network."
+    )
+    const nextButton = await contentElement.findElement(
+      By.xpath(
+        "//div[contains(@class, 'v-step__buttons')]/button[contains(@class, 'v-step__button-next')]"
+      )
+    )
     expect(nextButton, 'Next button should be visible').not.null
     const nextButtonText = await nextButton.getText()
     expect(nextButtonText, 'Next button text should be equal to "Next"').to.be.equal('Next')
