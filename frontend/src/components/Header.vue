9de8fcd707beff7595df66076ec4da7a6c6db8cd
Den <den@Deniss-MacBook-Pro.local>
2024-07-21 16:00:22 +0200

    improve feel of header buttons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { SunIcon, MoonIcon, PresentationChartLineIcon } from '@heroicons/vue/24/solid'
+import { PresentationChartLineIcon } from '@heroicons/vue/24/solid'
+import { SunIcon, MoonIcon } from '@heroicons/vue/16/solid'
 import { useUIStore, useAccountsStore } from '@/stores'
 import { shortenAddress } from '@/utils'
 import { RouterLink } from 'vue-router'
@@ -19,22 +20,29 @@ const showTutorial = () => {
     <header
         class="flex justify-between items-center p-2 border-b border-b-slate-500 dark:border-b-zinc-500 dark:bg-zinc-800">
         <RouterLink to="/">
-            <img v-if="uiStore.mode === 'light'" alt="GenLayer Logo" class="logo" src="@/assets/images/logo.png" width="125" height="125"/>
-            <img v-else alt="GenLayer Logo" class="logo" src="@/assets/images/logo_white.png" width="125" height="125"/>
+            <img v-if="uiStore.mode === 'light'" alt="GenLayer Logo" class="logo" src="@/assets/images/logo.png"
+                width="125" height="125" />
+            <img v-else alt="GenLayer Logo" class="logo" src="@/assets/images/logo_white.png" width="125"
+                height="125" />
         </RouterLink>
-        <div class="flex items-center" id="tutorial-end">
-            <RouterLink :to="{ name: 'profile' }" class="text-sm  text-primary dark:text-white">
-                {{ shortenAddress(accounts.currentUserAddress || '') }}
+        <div class="flex items-center gap-2 pr-2" id="tutorial-end">
+            <RouterLink :to="{ name: 'profile' }"
+                class="  hover:bg-slate-200 dark:hover:bg-zinc-700 rounded-md p-1 px-2">
+                <span class="text-sm text-primary dark:text-white font-medium">
+                    {{ shortenAddress(accounts.currentUserAddress || '') }}
+                </span>
                 <ToolTip :text="accounts.currentUserAddress" :options="{ placement: 'bottom' }" />
             </RouterLink>
 
-            <button class="mx-3" @click="toggleMode">
+            <button @click="toggleMode" class="hover:bg-slate-200 dark:hover:bg-zinc-700 rounded-md p-1">
                 <SunIcon v-if="uiStore.mode === 'light'" class="h-5 w-5 fill-gray-700" />
                 <MoonIcon v-else class="h-5 w-5 fill-gray-200" />
                 <ToolTip text="Switch Theme" :options="{ placement: 'bottom' }" />
             </button>
-            <button class="mx-3" @click="showTutorial">
-                <PresentationChartLineIcon class="h-5 w-5" :class="uiStore.mode === 'light' ? 'fill-gray-700' : 'fill-gray-200'" />
+
+            <button @click="showTutorial" class="hover:bg-slate-200 dark:hover:bg-zinc-700 rounded-md p-1">
+                <PresentationChartLineIcon class="h-5 w-5"
+                    :class="uiStore.mode === 'light' ? 'fill-gray-700' : 'fill-gray-200'" />
                 <ToolTip text="Show Tutorial" :options="{ placement: 'bottom' }" />
             </button>
         </div>
