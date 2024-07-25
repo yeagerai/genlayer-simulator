3fee58cb5521c14c6ba24e5d12da2206127a4698
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2024-07-25 15:07:01 -0300

    SIM-BE-Decouple configuration (#310)
    * refactor: use configuration object
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
    
    * improvement[gh-action]: sensible timeout
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
    
    * fix[BE]: configuration import and call
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
    
    * test[gh-action]: allow for workflow dispatch
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
    
    * fix[BE]: sort function call
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
    
    ---------
    
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- backend/node/create_nodes/create_nodes.py
+++ backend/node/create_nodes/create_nodes.py
@@ -1,6 +1,7 @@
 import os
 import json
 import re
+from typing import Callable
 import requests
 import numpy as np
 from random import random, choice, uniform
@@ -12,15 +13,13 @@ load_dotenv()
 default_provider_key_regex = r"<add_your_.*_api_key_here>"
 
 
-def get_ollama_url(endpoint: str) -> str:
-    return f"{os.environ['OLAMAPROTOCOL']}://{os.environ['OLAMAHOST']}:{os.environ['OLAMAPORT']}/api/{endpoint}"
-
-
 def base_node_json(provider: str, model: str) -> dict:
     return {"provider": provider, "model": model, "config": {}}
 
 
-def get_random_provider_using_weights(defaults: dict[str]) -> str:
+def get_random_provider_using_weights(
+    defaults: dict[str], get_ollama_url: Callable[[str], str]
+) -> str:
     # remove providers if no api key
     provider_weights: dict[str, float] = defaults["provider_weights"]
 
@@ -34,7 +33,10 @@ def get_random_provider_using_weights(defaults: dict[str]) -> str:
         or re.match(os.environ["HEURISTAIAPIKEY"])
     ):
         provider_weights.pop("heuristai")
-    if "ollama" in provider_weights and get_provider_models({}, "ollama") == []:
+    if (
+        "ollama" in provider_weights
+        and get_provider_models({}, "ollama", get_ollama_url) == []
+    ):
         provider_weights.pop("ollama")
 
     if len(provider_weights) == 0:
@@ -50,7 +52,9 @@ def get_random_provider_using_weights(defaults: dict[str]) -> str:
             return key
 
 
-def get_provider_models(defaults: dict, provider: str) -> list:
+def get_provider_models(
+    defaults: dict, provider: str, get_ollama_url: Callable[[str], str]
+) -> list:
 
     if provider == "ollama":
         ollama_models_result = requests.get(get_ollama_url("tags")).json()
@@ -116,14 +120,16 @@ def num_decimal_places(number: float) -> int:
     return decimal_places
 
 
-def random_validator_config(providers: list = None):
+def random_validator_config(
+    get_ollama_url: Callable[[str], str], providers: list = None
+):
     providers = providers or []
 
     if len(providers) == 0:
         providers = get_providers()
     default_config = get_default_config_for_providers_and_nodes()
     config = get_config_with_specific_providers(default_config, providers)
-    ollama_models = get_provider_models({}, "ollama")
+    ollama_models = get_provider_models({}, "ollama", get_ollama_url)
 
     if (
         not len(ollama_models)
@@ -139,11 +145,13 @@ def random_validator_config(providers: list = None):
     # for entry in heuristic_models_result:
     #    heuristic_models.append(entry['name'])
 
-    provider = get_random_provider_using_weights(config["providers"])
+    provider = get_random_provider_using_weights(config["providers"], get_ollama_url)
     options = get_options(provider, config)
 
     if provider == "openai":
-        openai_model = choice(get_provider_models(config["providers"], "openai"))
+        openai_model = choice(
+            get_provider_models(config["providers"], "openai", get_ollama_url)
+        )
         node_config = base_node_json("openai", openai_model)
 
     elif provider == "ollama":
@@ -179,7 +187,9 @@ def random_validator_config(providers: list = None):
                 raise Exception("Option is not a dict or str (" + option + ")")
 
     elif provider == "heuristai":
-        heuristic_model = choice(get_provider_models(config["providers"], "heuristai"))
+        heuristic_model = choice(
+            get_provider_models(config["providers"], "heuristai", get_ollama_url)
+        )
         node_config = base_node_json("heuristai", heuristic_model)
         for option, option_config in options.items():
             if random() > config["providers"]["chance_of_default_value"]:
