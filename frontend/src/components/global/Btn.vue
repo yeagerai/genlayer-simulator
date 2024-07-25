32b65035d7b9edb1fea56e1e8652a3a38374a796
Den <den@Deniss-MacBook-Pro.local>
2024-07-25 12:44:55 +0200

    improve btn component styles

--- frontend/src/components/global/Btn.vue
+++ frontend/src/components/global/Btn.vue
@@ -27,13 +27,13 @@ const primary = computed(() => !props.secondary && !props.dangerous)
 <template>
   <button
     :id="id"
-    class="flex items-center justify-center gap-2 rounded-md px-4 py-2 font-semibold text-white transition-all hover:text-white active:scale-95"
+    class="flex items-center justify-center gap-2 rounded-md px-4 py-2 font-semibold transition-all active:scale-95"
     :class="[
-      primary && 'bg-primary hover:bg-opacity-90 dark:bg-accent',
+      primary && 'bg-primary text-white hover:bg-opacity-90 hover:text-white dark:bg-accent',
       secondary &&
-        'dark:hover:bg-white-100 border border-gray-300 bg-gray-100 text-gray-700 hover:bg-white hover:text-gray-700 dark:border-none dark:border-gray-400 dark:bg-transparent dark:bg-white dark:bg-opacity-10 dark:text-white dark:shadow-none dark:hover:bg-opacity-20 dark:hover:text-white',
-      dangerous && 'bg-red-600 hover:bg-red-500',
-      disabled && 'opacity-50 cursor-not-allowed active:scale-100',
+        'dark:hover:bg-white-100 border border-gray-300 bg-gray-100 text-gray-800 hover:bg-white hover:text-gray-800 dark:border-none dark:border-gray-400 dark:bg-transparent dark:bg-white dark:bg-opacity-10 dark:text-white dark:shadow-none dark:hover:bg-opacity-20 dark:hover:text-white',
+      dangerous && 'bg-red-600 text-white hover:bg-red-500 hover:text-white',
+      disabled && 'cursor-not-allowed opacity-50 active:scale-100',
     ]"
     :disabled="disabled || loading"
     @click="emit('click')"
