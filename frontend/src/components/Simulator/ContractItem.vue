bdf26e7739b6d52ecf0e1237bbbab2f4f081987a
Den <den@Deniss-MacBook-Pro.local>
2024-07-25 11:23:35 +0200

    fine tune name edition field in contract items

--- frontend/src/components/Simulator/ContractItem.vue
+++ frontend/src/components/Simulator/ContractItem.vue
@@ -55,27 +55,20 @@ const handleRemoveFile = () => {
 
 <template>
   <div class="flex w-full cursor-pointer flex-col">
-    <div
-      :class="[
-        'group flex items-center px-2 py-1.5 text-xs transition-colors hover:bg-gray-50',
-        isActive &&
-          'bg-gray-200 hover:bg-gray-200 dark:bg-zinc-600 dark:text-white hover:dark:bg-zinc-600',
-        !isActive &&
-          'bg-white text-neutral-500 hover:bg-gray-100 dark:bg-zinc-800 hover:dark:bg-zinc-700',
-      ]"
-    >
+    <div :class="[
+      'group flex items-center px-2 py-1.5 text-xs transition-colors hover:bg-gray-50',
+      isActive &&
+      'bg-gray-200 hover:bg-gray-200 dark:bg-zinc-600 dark:text-white hover:dark:bg-zinc-600',
+      !isActive &&
+      'bg-white text-neutral-500 hover:bg-gray-100 dark:bg-zinc-800 hover:dark:bg-zinc-700',
+    ]">
       <DocumentCheckIcon class="mr-1 h-4 w-4 fill-primary dark:fill-white" />
 
-      <div class="flex w-full items-center justify-between" v-if="isEditing">
-        <input
-          type="text"
-          ref="editInput"
-          class="w-full bg-slate-100 dark:bg-zinc-700"
-          v-model="editingFileName"
-          @blur="handleSaveFile"
-          @keydown.enter="handleSaveFile"
-          @keydown.escape="handleStopEditing"
-        />
+      <div class="w-full" v-if="isEditing">
+        <input type="text" ref="editInput"
+          class="font-semibold rounded-[1px] p-[1px] -m-[1px] w-full bg-slate-100 dark:bg-zinc-700 !shadow-none !border-none focus:outline focus:outline-gray-400"
+          v-model="editingFileName" @blur="handleSaveFile" @keydown.enter="handleSaveFile"
+          @keydown.escape="handleStopEditing" />
       </div>
 
       <div v-else class="flex w-full items-center justify-between truncate">
@@ -86,27 +79,20 @@ const handleRemoveFile = () => {
         <div class="hidden flex-row gap-1 group-hover:flex">
           <button @click="handleEditFile({ id: contract.id, name: contract.name })">
             <PencilSquareIcon
-              class="h-3 w-3 text-gray-400 transition-all hover:text-gray-800 active:scale-90 dark:hover:text-white"
-            />
+              class="h-3 w-3 text-gray-400 transition-all hover:text-gray-800 active:scale-90 dark:hover:text-white" />
             <ToolTip text="Edit Name" :options="{ placement: 'bottom' }" />
           </button>
 
           <button @click="deleteModalOpen = true">
             <TrashIcon
-              class="h-3 w-3 text-gray-400 transition-all hover:text-gray-800 active:scale-90 dark:hover:text-white"
-            />
+              class="h-3 w-3 text-gray-400 transition-all hover:text-gray-800 active:scale-90 dark:hover:text-white" />
             <ToolTip text="Delete file" :options="{ placement: 'bottom' }" />
           </button>
         </div>
       </div>
 
-      <ConfirmationModal
-        :open="deleteModalOpen"
-        @close="deleteModalOpen = false"
-        @confirm="handleRemoveFile"
-        buttonText="Delete Contract"
-        dangerous
-      >
+      <ConfirmationModal :open="deleteModalOpen" @close="deleteModalOpen = false" @confirm="handleRemoveFile"
+        buttonText="Delete Contract" dangerous>
         <template #title>Delete Contract</template>
         <template #description>Are you sure you want to delete this contract?</template>
         <template #info>{{ contract.name }}</template>
