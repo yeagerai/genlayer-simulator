b47405f1880b0e72e8ad64ba035ab0f78d6437cb
Den <den@Deniss-MacBook-Pro.local>
2024-07-26 12:45:32 +0200

    make section with one root element to allow passing ids

--- frontend/src/components/Simulator/PageSection.vue
+++ frontend/src/components/Simulator/PageSection.vue
@@ -1,15 +1,17 @@
 <template>
-  <div
-    class="flex w-full flex-row items-center justify-between bg-slate-100 p-2 dark:dark:bg-zinc-700"
-  >
-    <h4 class="text-md">
-      <slot name="title" />
-    </h4>
+  <div>
+    <div
+      class="flex w-full flex-row items-center justify-between bg-slate-100 p-2 dark:dark:bg-zinc-700"
+    >
+      <h4 class="text-md">
+        <slot name="title" />
+      </h4>
 
-    <slot name="actions" />
-  </div>
+      <slot name="actions" />
+    </div>
 
-  <div class="flex w-full flex-col p-2 gap-2">
-    <slot />
+    <div class="flex w-full flex-col gap-2 p-2">
+      <slot />
+    </div>
   </div>
 </template>
