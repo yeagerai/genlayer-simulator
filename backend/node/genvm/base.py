1e4930c705f6be7695e15361c0168422545e24dc
Cristiam Da Silva <cristiam86@gmail.com>
2024-07-26 14:30:53 +0200

    259 sim implement VectorStore for llm embeddings (#276)
    * feat: added vector store class
    
    * feat: added requirements
    
    * fix: removed extra logging
    
    * feat: added simple logging for transaction update
    
    * feat: added vector storage class
    
    * feat: added simple example of IC using the new vector storage
    
    * fix: truncate longer log messages
    
    * refactor: added max log length config
    
    * fix: performance issues by avoiding storing the model in the contract state
    
    * fix: avoid side effects with global variables
    
    * feat: added example intelligent contract for VectorStore usage
    
    * refactor: renamed example
    
    * fix: removed unused param
    
    * test: added vector store contract tests
    
    * fix: vector store bugs and improvements
    
    * persist and return text
    * keep similarity <1
    * fix vectorstore id assignment
    * fix empty vectorstore similarity search
    * add typing
    * enable getting all items
    
    ---------
    
    Co-authored-by: Edgars <edgars.nemse@gmail.com>

--- backend/node/genvm/base.py
+++ backend/node/genvm/base.py
@@ -7,10 +7,27 @@ import asyncio
 import pickle
 import base64
 import sys
+from contextlib import contextmanager
 
 from backend.database_handler.contract_snapshot import ContractSnapshot
 from backend.node.genvm.equivalence_principle import EquivalencePrinciple
 from backend.node.genvm.code_enforcement import code_enforcement_check
+from backend.node.genvm.std.vector_store import VectorStore
+
+
+@contextmanager
+def safe_globals():
+    old_globals = globals().copy()
+    globals().update(
+        {
+            "contract_runner": None,
+            "VectorStore": VectorStore,
+        }
+    )
+    try:
+        yield
+    finally:
+        globals().update(old_globals)
 
 
 class ContractRunner:
@@ -72,24 +89,24 @@ class GenVM:
         code_enforcement_check(code_to_deploy, class_name)
         self.contract_runner.from_address = from_address
 
-        local_namespace = {}
-        globals()["contract_runner"] = self.contract_runner
-        exec(code_to_deploy, globals(), local_namespace)
+        with safe_globals():
+            globals()["contract_runner"] = self.contract_runner
+            local_namespace = {}
+            exec(code_to_deploy, globals(), local_namespace)
 
-        class_name = self._get_contract_class_name(code_to_deploy)
-        contract_class = local_namespace[class_name]
+            class_name = self._get_contract_class_name(code_to_deploy)
+            contract_class = local_namespace[class_name]
 
-        module = sys.modules[__name__]
-        setattr(module, class_name, contract_class)
+            module = sys.modules[__name__]
+            setattr(module, class_name, contract_class)
 
-        current_contract = contract_class(**constructor_args)
+            current_contract = contract_class(**constructor_args)
 
-        pickled_object = pickle.dumps(current_contract)
-        encoded_pickled_object = base64.b64encode(pickled_object).decode("utf-8")
+            pickled_object = pickle.dumps(current_contract)
+            encoded_pickled_object = base64.b64encode(pickled_object).decode("utf-8")
 
-        ## Clean up
-        del globals()["contract_runner"]
-        delattr(module, class_name)
+            ## Clean up
+            delattr(module, class_name)
 
         return self._generate_receipt(
             class_name, encoded_pickled_object, "__init__", [constructor_args]
@@ -101,35 +118,38 @@ class GenVM:
         self.contract_runner.from_address = from_address
         contract_code = self.snapshot.contract_code
 
-        local_namespace = {}
-        # Execute the code to ensure all classes are defined in the local_namespace
-        exec(contract_code, globals(), local_namespace)
+        with safe_globals():
+            globals()["contract_runner"] = self.contract_runner
+            local_namespace = {}
+            # Execute the code to ensure all classes are defined in the local_namespace
+            exec(contract_code, globals(), local_namespace)
 
-        # Ensure the class and other necessary elements are in the global local_namespace if needed
-        for name, value in local_namespace.items():
-            globals()[name] = value
+            # Ensure the class and other necessary elements are in the global local_namespace if needed
+            for name, value in local_namespace.items():
+                globals()[name] = value
 
-        globals()["contract_runner"] = self.contract_runner
+            self.eq_principle.contract_runner = self.contract_runner
 
-        self.eq_principle.contract_runner = self.contract_runner
+            contract_encoded_state = self.snapshot.encoded_state
+            decoded_pickled_object = base64.b64decode(contract_encoded_state)
+            current_contract = pickle.loads(decoded_pickled_object)
 
-        contract_encoded_state = self.snapshot.encoded_state
-        decoded_pickled_object = base64.b64decode(contract_encoded_state)
-        current_contract = pickle.loads(decoded_pickled_object)
+            if self.contract_runner.mode == "validator":
+                leader_receipt_eq_result = leader_receipt["result"]["eq_outputs"][
+                    "leader"
+                ]
+                self.contract_runner.eq_outputs["leader"] = leader_receipt_eq_result
 
-        if self.contract_runner.mode == "validator":
-            leader_receipt_eq_result = leader_receipt["result"]["eq_outputs"]["leader"]
-            self.contract_runner.eq_outputs["leader"] = leader_receipt_eq_result
+            function_to_run = getattr(current_contract, function_name, None)
+            if asyncio.iscoroutinefunction(function_to_run):
+                await function_to_run(*args)
+            else:
+                function_to_run(*args)
 
-        function_to_run = getattr(current_contract, function_name, None)
-        if asyncio.iscoroutinefunction(function_to_run):
-            await function_to_run(*args)
-        else:
-            function_to_run(*args)
+            pickled_object = pickle.dumps(current_contract)
+            encoded_pickled_object = base64.b64encode(pickled_object).decode("utf-8")
+            class_name = self._get_contract_class_name(contract_code)
 
-        pickled_object = pickle.dumps(current_contract)
-        encoded_pickled_object = base64.b64encode(pickled_object).decode("utf-8")
-        class_name = self._get_contract_class_name(contract_code)
         return self._generate_receipt(
             class_name, encoded_pickled_object, function_name, [args]
         )
@@ -138,53 +158,57 @@ class GenVM:
     def get_contract_schema(contract_code: str) -> dict:
 
         namespace = {}
-        exec(contract_code, globals(), namespace)
-        class_name = GenVM._get_contract_class_name(contract_code)
+        with safe_globals():
+            exec(contract_code, globals(), namespace)
+            class_name = GenVM._get_contract_class_name(contract_code)
 
-        iclass = namespace[class_name]
+            iclass = namespace[class_name]
 
-        members = inspect.getmembers(iclass)
+            members = inspect.getmembers(iclass)
 
-        # Find all class methods
-        methods = {}
-        functions_and_methods = [
-            m for m in members if inspect.isfunction(m[1]) or inspect.ismethod(m[1])
-        ]
-        for name, member in functions_and_methods:
-            signature = inspect.signature(member)
+            # Find all class methods
+            methods = {}
+            functions_and_methods = [
+                m for m in members if inspect.isfunction(m[1]) or inspect.ismethod(m[1])
+            ]
+            for name, member in functions_and_methods:
+                signature = inspect.signature(member)
 
-            inputs = {}
-            for method_variable_name, method_variable in signature.parameters.items():
-                if method_variable_name != "self":
-                    annotation = str(method_variable.annotation)[8:-2]
-                    inputs[method_variable_name] = str(annotation)
+                inputs = {}
+                for (
+                    method_variable_name,
+                    method_variable,
+                ) in signature.parameters.items():
+                    if method_variable_name != "self":
+                        annotation = str(method_variable.annotation)[8:-2]
+                        inputs[method_variable_name] = str(annotation)
 
-            return_annotation = str(signature.return_annotation)[8:-2]
+                return_annotation = str(signature.return_annotation)[8:-2]
 
-            if return_annotation == "inspect._empty":
-                return_annotation = "None"
+                if return_annotation == "inspect._empty":
+                    return_annotation = "None"
 
-            result = {"inputs": inputs, "output": return_annotation}
+                result = {"inputs": inputs, "output": return_annotation}
 
-            methods[name] = result
+                methods[name] = result
 
-        # Find all class variables
-        variables = {}
-        tree = ast.parse(contract_code)
-        for node in tree.body:
-            if isinstance(node, ast.ClassDef):
-                for stmt in node.body:
-                    if isinstance(stmt, ast.AnnAssign):
-                        if hasattr(stmt.annotation, "id") and hasattr(
-                            stmt.target, "id"
-                        ):
-                            variables[stmt.target.id] = stmt.annotation.id
+            # Find all class variables
+            variables = {}
+            tree = ast.parse(contract_code)
+            for node in tree.body:
+                if isinstance(node, ast.ClassDef):
+                    for stmt in node.body:
+                        if isinstance(stmt, ast.AnnAssign):
+                            if hasattr(stmt.annotation, "id") and hasattr(
+                                stmt.target, "id"
+                            ):
+                                variables[stmt.target.id] = stmt.annotation.id
 
-        contract_schema = {
-            "class": class_name,
-            "methods": methods,
-            "variables": variables,
-        }
+            contract_schema = {
+                "class": class_name,
+                "methods": methods,
+                "variables": variables,
+            }
 
         return contract_schema
 
@@ -193,21 +217,22 @@ class GenVM:
         code: str, state: str, method_name: str, method_args: list
     ) -> dict:
         namespace = {}
-        # Execute the code to ensure all classes are defined in the namespace
-        exec(code, globals(), namespace)
+        with safe_globals():
+            # Execute the code to ensure all classes are defined in the namespace
+            exec(code, globals(), namespace)
 
-        # Ensure the class and other necessary elements are in the global namespace if needed
-        for name, value in namespace.items():
-            globals()[name] = value
+            # Ensure the class and other necessary elements are in the global namespace if needed
+            for name, value in namespace.items():
+                globals()[name] = value
 
-        decoded_pickled_object = base64.b64decode(state)
-        contract_state = pickle.loads(decoded_pickled_object)
+            decoded_pickled_object = base64.b64decode(state)
+            contract_state = pickle.loads(decoded_pickled_object)
 
-        method_to_call = getattr(contract_state, method_name)
-        result = method_to_call(*method_args)
+            method_to_call = getattr(contract_state, method_name)
+            result = method_to_call(*method_args)
 
-        # Clean up
-        for name in namespace.keys():
-            del globals()[name]
+            # Clean up
+            for name in namespace.keys():
+                del globals()[name]
 
         return result
