92e947412656861e2ea0c6232b5d4434e10120f7
Den <den@Deniss-MacBook-Pro.local>
2024-07-26 12:32:38 +0200

    fix reserved param

--- frontend/src/components/global/FieldLabel.vue
+++ frontend/src/components/global/FieldLabel.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-const props = defineProps<{
-  for?: string
-}>()
-</script>
-
 <template>
-  <label :for="for" class="block text-sm font-medium leading-8"><slot /></label>
+  <label class="block text-sm font-medium leading-8"><slot /></label>
 </template>
