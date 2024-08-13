f009ef9d7a56dd6b1eb9efd02ee1b68b08db95d1
Cristiam Da Silva <cristiam86@gmail.com>
2024-08-13 16:20:26 +0200

    refactor: reverted enabling of tracking by default and having it enabled only when not in develop mode

--- frontend/src/hooks/useEventTracking.ts
+++ frontend/src/hooks/useEventTracking.ts
@@ -2,10 +2,7 @@ import { usePlausible } from 'v-plausible/vue';
 import { type EventName, type EventProperties } from '@/types';
 
 export const useEventTracking = () => {
-  // This enables/disables the tracking of events
-  // It could be defined based on the environment or a user setting like
-  // `import.meta.env.MODE === 'development';`
-  const isTrackingEnabled = true;
+  const isDevelopment = import.meta.env.MODE === 'development';
   const { trackEvent: trackPlausibleEvent } = usePlausible();
 
   const trackEvent = <T extends EventName>(
@@ -13,14 +10,13 @@ export const useEventTracking = () => {
     properties?: EventProperties[T],
   ) => {
     try {
-      console.debug(
-        `Track Event${isTrackingEnabled ? '' : ' (blocked in dev mode)'}`,
-        {
+      if (isDevelopment) {
+        console.debug('Track Event (blocked in dev mode)', {
           name,
           properties,
-        },
-      );
-      if (isTrackingEnabled) {
+        });
+      } else {
+        console.debug('Track Event', { name, properties });
         trackPlausibleEvent(name, { props: properties });
       }
     } catch (err) {
