f69d96344a4800fed470aba16d80142e50fb92ce
Den <den@Deniss-MacBook-Pro.local>
2024-08-13 12:19:24 +0200

    disable in dev mode

--- frontend/src/hooks/useEventTracking.ts
+++ frontend/src/hooks/useEventTracking.ts
@@ -2,13 +2,12 @@ import { usePlausible } from 'v-plausible/vue';
 import { type EventName, type EventProperties } from '@/types';
 
 export const useEventTracking = () => {
-  const isDevelopment = false; // TODO:
-  // const isDevelopment = import.meta.env.MODE === 'development';
+  const isDevelopment = import.meta.env.MODE === 'development';
   const { trackEvent: trackPlausibleEvent } = usePlausible();
 
   const trackEvent = <T extends EventName>(
     name: T,
-    properties?: EventProperties[T]
+    properties?: EventProperties[T],
   ) => {
     try {
       if (isDevelopment) {
