0fccc46a290c3e4dd602f73b5dc377738d587956
den <26140541+denishacquin@users.noreply.github.com>
2024-08-15 11:39:58 +0200

    374 sim improve transactions info in the details modal (#383)
    * feat: improve the UI of transaction modal
    
    * fix: prevent double events on close modals
    
    * fix: lint errors
    
    * fix: improved output section title and scroll
    
    ---------
    
    Co-authored-by: Cristiam Da Silva <cristiam86@gmail.com>
    Co-authored-by: Den <den@Deniss-MacBook-Pro.local>

---
+++ frontend/src/components/Simulator/ModalSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="relative mb-2 flex flex-row items-center gap-2">
+      <div
+        class="flex flex-row items-center gap-2 whitespace-nowrap font-semibold"
+      >
+        <slot name="title" />
+      </div>
+
+      <div class="w-full border-t border-gray-300 dark:border-gray-500" />
+    </div>
+
+    <slot />
+  </div>
+</template>
