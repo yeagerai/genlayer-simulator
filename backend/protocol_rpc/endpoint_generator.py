b92ae2e7e67281805a0e15adbe2352dddc05da3f
Cristiam Da Silva <cristiam86@gmail.com>
2024-08-15 03:47:27 +0200

    376 sim allow to disable starting and success logs on rpc endpoints (#378)
    * refactor: enable event tracking by default
    
    * refactor: reverted enabling of tracking by default and having it enabled only when not in develop mode
    
    * fix: contract example for VectorStore
    
    * chore: log also when a transaction is updated as finalized
    
    * feat: Added filter list to print starting and success execution logs
    
    * feat: set log level and endpoints with info logs disabled in the environment
    
    * fix: removed unused imports

--- backend/protocol_rpc/endpoint_generator.py
+++ backend/protocol_rpc/endpoint_generator.py
@@ -1,24 +1,49 @@
 # rpc/endpoint_generator.py
-
+import traceback
 from functools import partial, wraps
 from typing import Callable
 from flask_jsonrpc import JSONRPC
+
+from backend.protocol_rpc.configuration import GlobalConfiguration
 from backend.protocol_rpc.message_handler.base import MessageHandler
+from backend.protocol_rpc.types import EndpointResult, EndpointResultStatus
 
 
 def generate_rpc_endpoint(
-    jsonrpc: JSONRPC, msg_handler: MessageHandler, function: Callable
+    jsonrpc: JSONRPC,
+    msg_handler: MessageHandler,
+    config: GlobalConfiguration,
+    function: Callable,
 ) -> Callable:
     @jsonrpc.method(function.__name__)
     @wraps(function)
     def endpoint(*args, **kwargs):
+        shouldPrintInfoLogs = (
+            function.__name__ not in config.get_disabled_info_logs_endpoints()
+        )
         send_message = partial(msg_handler.send_message, function.__name__)
-        send_message("info", "Starting...")
+        if shouldPrintInfoLogs:
+            send_message(EndpointResult(EndpointResultStatus.INFO, "Starting..."))
+
         try:
-            result = function(*args, **kwargs)
-            return send_message("success", data=result)
+            function_result = function(*args, **kwargs)
+            result = EndpointResult(
+                EndpointResultStatus.SUCCESS,
+                f"Endpoint {function.__name__} successfully executed",
+                function_result,
+            )
+            if shouldPrintInfoLogs:
+                send_message(result)
+            return result.to_json()
         except Exception as e:
-            return send_message("error", exception=str(e))
+            result = EndpointResult(
+                EndpointResultStatus.ERROR,
+                f"Error executing endpoint {function.__name__}: {str(e)}",
+                {"traceback": traceback.format_exc()},
+                e,
+            )
+            send_message(result)
+            return result.to_json()
 
     return endpoint
 
