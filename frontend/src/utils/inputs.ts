1b9fae155681408e1f46d32f71734b4b935dd3ee
Den <den@deniss-mbp.home>
2024-08-19 11:19:48 +0200

    perf: cleanup backwards compatibility code for constructors

--- frontend/src/utils/inputs.ts
+++ frontend/src/utils/inputs.ts
@@ -4,8 +4,6 @@ import FloatField from '@/components/global/fields/FloatField.vue';
 import BooleanField from '@/components/global/fields/BooleanField.vue';
 
 export const InputTypesMap: { [k: string]: any } = {
-  str: StringField, // For backwards compatibility with constructors; should be removed when constructors are refactored
-  int: IntegerField, // For backwards compatibility with constructors; should be removed when constructors are refactored
   string: StringField,
   uint256: IntegerField,
   float: FloatField,
