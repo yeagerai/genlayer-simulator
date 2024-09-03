a11b8ef69a4d2a6c0bc9165c055a2140256fd37c
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-09-03 14:10:04 -0300

    add jsf for json schema
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- backend/node/create_nodes/test_create_nodes.py
+++ backend/node/create_nodes/test_create_nodes.py
@@ -1,4 +1,5 @@
-from backend.node.create_nodes.create_nodes import (
+import os
+from create_nodes import (
     get_default_config_for_providers_and_nodes,
 )
 
@@ -10,3 +11,15 @@ def test_get_default_config_for_providers_and_nodes():
     assert "providers" in out
     assert "provider_weights" in out["providers"]
     assert "node_defaults" in out
+
+
+def test():
+    from jsf import JSF
+
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    schema_file = os.path.join(current_directory, "providers_schema.json")
+    faker = JSF.from_json(schema_file)
+
+    fake_json = faker.generate()
+
+    print(fake_json)
