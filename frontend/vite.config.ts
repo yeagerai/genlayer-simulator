87819023974feb3ee2016e16bf2673373e133e10
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-09-03 11:32:58 -0300

    fix vite config
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,43 @@
 import { fileURLToPath, URL } from 'node:url';
 import svgLoader from 'vite-svg-loader';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [vue(), svgLoader(), vueJsx(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:8080',
-    proxy: {
-      '/api': {
-        target: 'http://jsonrpc:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const config: UserConfig = {
+    base: '/',
+    plugins: [vue(), svgLoader(), vueJsx(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    preview: {
+      port: 8080,
+      strictPort: true,
+    },
+    server: {
+      port: 8080,
+      strictPort: true,
+      host: true,
+      origin: 'http://0.0.0.0:8080',
+      proxy:
+        env.VITE_PROXY_ENABLED !== 'true'
+          ? undefined
+          : {
+              '/api': {
+                target: env.VITE_PROXY_JSON_RPC_SERVER_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+              },
+            },
+    },
+  };
+
+  return config;
 });
