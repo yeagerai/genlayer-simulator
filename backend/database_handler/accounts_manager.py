e762291e314a9c2f48a51e1c592a2e646fede9fa
Cristiam Da Silva <cristiam86@gmail.com>
2024-09-09 13:59:28 +0200

    feat: added balance management methods

--- backend/database_handler/accounts_manager.py
+++ backend/database_handler/accounts_manager.py
@@ -5,20 +5,19 @@ from eth_account._utils.validation import is_valid_address
 
 from .models import CurrentState
 from backend.database_handler.errors import AccountNotFoundError
-from backend.database_handler.transactions_processor import TransactionsProcessor
 
 from sqlalchemy.orm import Session
 
 
 class AccountsManager:
-    def __init__(self, session: Session, transactions_processor: TransactionsProcessor):
+    def __init__(self, session: Session):
         self.session = session
-        self.transactions_processor = transactions_processor
 
     def _parse_account_data(self, account_data: CurrentState) -> dict:
         return {
             "id": account_data.id,
             "data": account_data.data,
+            "balance": account_data.balance,
             "updated_at": account_data.updated_at.isoformat(),
         }
 
@@ -34,7 +33,7 @@ class AccountsManager:
     def create_new_account_with_address(self, address: str):
         if not self.is_valid_address(address):
             raise ValueError(f"Invalid address: {address}")
-        account_state = CurrentState(id=address)
+        account_state = CurrentState(id=address, data={})
         self.session.add(account_state)
         self.session.commit()
 
@@ -60,11 +59,16 @@ class AccountsManager:
         return self._parse_account_data(account_data)
 
     def get_account_balance(self, account_address: str) -> int:
-        account = self.get_account_or_fail(account_address)
+        account = self.get_account(account_address)
+        if not account:
+            return 0
         return account.balance
 
     def update_account_balance(self, account_address: str, new_balance: int):
-        to_account = self.get_account_or_fail(account_address)
+        to_account = self.get_account(account_address)
+        if to_account is None:
+            self.create_new_account_with_address(account_address)
+            to_account = self.get_account(account_address)
         to_account.balance = new_balance
         self.session.commit()
 
