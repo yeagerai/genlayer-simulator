0461bf1d48ce842833a37410fab415330db2bdd8
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-09-12 10:54:43 -0300

    improve schema
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- tests/unit/test_providers.py
+++ tests/unit/test_providers.py
@@ -1,9 +1,75 @@
-from backend.node.create_nodes.providers import (
-    get_default_providers,
-)
+import pytest
+from backend.domain.types import LLMProvider
+from backend.node.create_nodes.providers import get_default_providers, validate_provider
 
 
 def test_default_providers_valid():
     providers = get_default_providers()
 
     assert len(providers) > 0
+
+
+@pytest.mark.parametrize(
+    "llm_provider",
+    [
+        pytest.param(
+            LLMProvider(
+                plugin="openai",
+                provider="custom provider",
+                model="custom model",
+                config="<empty>",
+                plugin_config={
+                    "api_key_env_var": "some api key",
+                },
+            ),
+            id="custom openai",
+        ),
+        pytest.param(
+            LLMProvider(
+                plugin="heuristai",
+                provider="custom provider",
+                model="custom model",
+                config={
+                    "max_tokens": 100,
+                    "temperature": 0.5,
+                },
+                plugin_config={
+                    "api_key_env_var": "some api key",
+                    "api_url": "http://localhost:8000",
+                },
+            ),
+            id="custom heuristai",
+        ),
+        pytest.param(
+            LLMProvider(
+                plugin="ollama",
+                provider="custom provider",
+                model="custom model",
+                config={
+                    "mirostat": 0,
+                    "mirostat_eta": 0.1,
+                    "microstat_tau": 5,
+                    "num_ctx": 2048,
+                    "num_qga": 8,
+                    "num_gpu": 0,
+                    "num_thread": 2,
+                    "repeat_last_n": 64,
+                    "repeat_penalty": 1.1,
+                    "temprature": 0.8,
+                    "seed": 0,
+                    "stop": "",
+                    "tfs_z": 1.0,
+                    "num_predict": 128,
+                    "top_k": 40,
+                    "top_p": 0.9,
+                },
+                plugin_config={
+                    "api_url": "http://localhost:8000",
+                },
+            ),
+            id="custom ollama",
+        ),
+    ],
+)
+def test_validate_provider(llm_provider):
+    validate_provider(llm_provider)
