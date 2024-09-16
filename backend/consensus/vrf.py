c1efe631ef50245a1480c1a3c723462506916890
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-09-16 10:10:46 -0300

    fix tests
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- backend/consensus/vrf.py
+++ backend/consensus/vrf.py
@@ -1,23 +1,24 @@
+from datetime import datetime
 import numpy as np
-
-rng = np.random.default_rng()
 
 
 def get_validators_for_transaction(
-    all_validators: list[dict], num_validators: int
-) -> tuple:
-    num_validators = min(num_validators, len(all_validators))
+    nodes: list[dict],
+    num_validators: int,
+    rng=np.random.default_rng(seed=int(datetime.now().timestamp())),
+) -> tuple[dict, list[dict]]:
+    num_validators = min(num_validators, len(nodes))
 
-    total_stake = sum(validator["stake"] for validator in all_validators)
-    probabilities = [validator["stake"] / total_stake for validator in all_validators]
+    total_stake = sum(validator["stake"] for validator in nodes)
+    probabilities = [validator["stake"] / total_stake for validator in nodes]
 
     selected_validators = rng.choice(
-        all_validators,
+        nodes,
         p=probabilities,
         size=num_validators,
         replace=False,
     )
 
     leader = selected_validators[0]
-    remaining_validators = selected_validators[1 : num_validators + 1]
+    remaining_validators = list(selected_validators[1 : num_validators + 1])
     return leader, remaining_validators
--- tests/unit/test_vrf.py
+++ tests/unit/test_vrf.py
@@ -1,25 +1,48 @@
 from backend.consensus.vrf import get_validators_for_transaction
 
 
+def list_of_dicst_to_set(list_of_dicts: list[dict]) -> set:
+    return set(map(lambda x: tuple(x.items()), list_of_dicts))
+
+
 def test_get_validators_for_transaction():
-    validators = [{"stake": 1}, {"stake": 2}, {"stake": 3}]
-    result = get_validators_for_transaction(validators, 10)
+    """
+    Tests that
+    * correctly returns all nodes when asked for more validators than there are nodes
+    * the order of the validators is random
 
-    assert set(result) == set(validators)
+    """
+    nodes = [{"stake": 1}, {"stake": 2}, {"stake": 3}]
+    nodes_set = list_of_dicst_to_set(nodes)
 
-    print(result)
+    while True:
+        leader, validators = get_validators_for_transaction(nodes, 10)
+        leader_and_validators = [leader] + validators
+        assert list_of_dicst_to_set(leader_and_validators) == nodes_set
+
+        if nodes != leader_and_validators:
+            # Since the order is random, at some point the order will be different
+            break
+
+    print(leader)
+    print(validators)
 
 
 def test_get_validators_for_transaction_2():
-    validators = [{"stake": 1}, {"stake": 2}, {"stake": 3}]
+    """
+    Tests that random selection should at some point return all nodes
+    """
 
-    validators_set = set(validators)
+    nodes = [{"stake": 1}, {"stake": 2}, {"stake": 3}]
+
+    nodes_set = list_of_dicst_to_set(nodes)
 
     accumulated = set()
     while True:
-        result = get_validators_for_transaction(validators, 2)
-        print(result)
-        accumulated.update(set(result))
+        leader, validators = get_validators_for_transaction(nodes, 2)
+        print(leader)
+        print(validators)
+        accumulated.update(list_of_dicst_to_set([leader] + validators))
 
-        if accumulated == set(validators_set):
+        if accumulated == nodes_set:
             break
