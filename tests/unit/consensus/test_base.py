ae2581d507ecb0e16f4a013473b933a7e04b4476
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-09-17 13:06:56 -0300

    add tests
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- tests/unit/consensus/test_base.py
+++ tests/unit/consensus/test_base.py
@@ -281,11 +281,6 @@ async def test_exec_transaction_no_consensus():
     for node in created_nodes:
         node.exec_transaction.assert_awaited_once_with(transaction)
 
-    assert (
-        transactions_processor.get_transaction_by_id(transaction.id)["status"]
-        == TransactionStatus.UNDETERMINED
-    )
-
     assert transactions_processor.updated_transaction_status_history == {
         "transaction_id": [
             TransactionStatus.PROPOSING,  # leader 1
@@ -300,6 +295,118 @@ async def test_exec_transaction_no_consensus():
             TransactionStatus.UNDETERMINED,  # all disagree
         ]
     }
+
+
+@pytest.mark.asyncio
+async def test_exec_transaction_one_disagreement():
+    """
+    Scenario: first round is disagreement, second round is agreement
+    Tests that consensus algorithm correctly rotates the leader when majority of nodes disagree
+    """
+
+    transaction = Transaction(
+        id="transaction_id",
+        from_address="from_address",
+        to_address="to_address",
+        status=TransactionStatus.PENDING,
+        type=TransactionType.RUN_CONTRACT,
+    )
+
+    nodes = [
+        {
+            "address": "address1",
+            "stake": 1,
+            "provider": "provider1",
+            "model": "model1",
+            "config": "config1",
+        },
+        {
+            "address": "address2",
+            "stake": 2,
+            "provider": "provider2",
+            "model": "model2",
+            "config": "config2",
+        },
+        {
+            "address": "address3",
+            "stake": 3,
+            "provider": "provider3",
+            "model": "model3",
+            "config": "config3",
+        },
+    ]
+
+    created_nodes = []
+
+    def node_factory(
+        node: dict,
+        mode: ExecutionMode,
+        contract_snapshot: ContractSnapshot,
+        receipt: Receipt | None,
+        msg_handler: MessageHandler,
+    ):
+        mock = Mock(Node)
+
+        mock.validator_mode = mode
+        mock.address = node["address"]
+        mock.leader_receipt = receipt
+
+        async def exec_transaction(transaction: Transaction):
+            return Receipt(
+                vote=(
+                    Vote.AGREE
+                    if ((len(created_nodes) - 1) // len(nodes))
+                    == 1  # only agree in the second round
+                    else Vote.DISAGREE
+                ),
+                class_name="",
+                args=[],
+                mode=mode,
+                method="",
+                gas_used=0,
+                contract_state="",
+                node_config={},
+                eq_outputs={},
+                execution_result=ExecutionResultStatus.SUCCESS,
+                error=None,
+            )
+
+        mock.exec_transaction.side_effect = exec_transaction
+
+        created_nodes.append(mock)
+
+        return mock
+
+    transactions_processor = TransactionsProcessorMock(
+        [transaction_to_dict(transaction)]
+    )
+
+    await ConsensusAlgorithm(None, None).exec_transaction(
+        transaction=transaction,
+        transactions_processor=transactions_processor,
+        snapshot=SnapshotMock(nodes),
+        accounts_manager=AccountsManagerMock(),
+        contract_snapshot_factory=contract_snapshot_factory,
+        node_factory=node_factory,
+    )
+
+    assert transactions_processor.updated_transaction_status_history == {
+        "transaction_id": [
+            TransactionStatus.PROPOSING,  # leader 1
+            TransactionStatus.COMMITTING,
+            TransactionStatus.REVEALING,
+            TransactionStatus.PROPOSING,  # rotation, leader 2
+            TransactionStatus.COMMITTING,
+            TransactionStatus.REVEALING,
+            TransactionStatus.ACCEPTED,
+            TransactionStatus.FINALIZED,
+        ]
+    }
+
+    assert len(created_nodes) == len(nodes) * 2
+
+    for node in created_nodes:
+        node.exec_transaction.assert_awaited_once_with(transaction)
 
 
 def test_rotate():
