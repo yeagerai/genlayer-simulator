fa2f49de7c2803928d99c5228ac595bbd58ca443
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-10-04 08:30:08 -0300

    fix e2e test
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- tests/unit/test_endpoint_generator.py
+++ tests/unit/test_endpoint_generator.py
@@ -13,3 +13,7 @@ def test_serialize():
     assert _serialize(dataclass()) == {}
     assert _serialize("test") == "test"
     assert _serialize((1, 2, ["test", False, ()])) == [1, 2, ["test", False, []]]
+    assert _serialize({"test": 1, "test2": {"test3": "test4"}}) == {
+        "test": 1,
+        "test2": {"test3": "test4"},
+    }
