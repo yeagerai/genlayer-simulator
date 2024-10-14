4d7b7ad79e41579d64a3e9466f5ff956496d4856
Den <den@deniss-macbook-pro.home>
2024-10-14 17:30:33 +0100

    fix: close account select on activating account

--- frontend/src/components/Simulator/AccountItem.vue
+++ frontend/src/components/Simulator/AccountItem.vue
@@ -44,52 +44,54 @@ const deleteModalOpen = ref(false);
 </script>
 
 <template>
-  <button
-    class="group flex w-full flex-row items-center justify-between gap-2 truncate bg-slate-100 p-2 text-left hover:bg-slate-200 dark:bg-gray-700 dark:hover:bg-gray-600"
-    @click="setCurentAddress()"
-  >
-    <div>
-      <PowerCircle
-        v-if="!active"
-        class="h-4 w-4 text-gray-400 opacity-0 transition-all group-hover:opacity-100"
-      />
-      <PowerCircle class="h-4 w-4 text-green-500" v-if="active" />
-    </div>
-
-    <span
-      class="flex grow flex-row truncate font-mono text-xs font-semibold"
-      :class="[!active && 'opacity-50']"
+  <div>
+    <button
+      class="group flex w-full flex-row items-center justify-between gap-2 truncate bg-slate-100 p-2 text-left hover:bg-slate-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+      @click="setCurentAddress()"
     >
-      {{ store.accountFromPrivateKey(privateKey).address }}
-    </span>
-
-    <CopyTextButton
-      :text="store.accountFromPrivateKey(privateKey).address"
-      class="opacity-0 group-hover:opacity-100"
-    />
-
-    <TrashIcon
-      v-if="canDelete"
-      class="h-4 w-4 shrink-0 text-gray-400 opacity-0 transition-all hover:text-red-500 group-hover:opacity-100"
-      @click.stop="deleteModalOpen = true"
-    />
-  </button>
-
-  <ConfirmationModal
-    :open="deleteModalOpen"
-    @close="deleteModalOpen = false"
-    @confirm="deleteAddress"
-    buttonText="Delete account"
-    dangerous
-  >
-    <template #title>Delete Account</template>
-    <template #description
-      >Are you sure you want to delete this account?</template
-    >
-    <template #info>
-      <div class="font-mono text-xs">
-        {{ store.accountFromPrivateKey(privateKey).address }}
+      <div>
+        <PowerCircle
+          v-if="!active"
+          class="h-4 w-4 text-gray-400 opacity-0 transition-all group-hover:opacity-100"
+        />
+        <PowerCircle class="h-4 w-4 text-green-500" v-if="active" />
       </div>
-    </template>
-  </ConfirmationModal>
+
+      <span
+        class="flex grow flex-row truncate font-mono text-xs font-semibold"
+        :class="[!active && 'opacity-50']"
+      >
+        {{ store.accountFromPrivateKey(privateKey).address }}
+      </span>
+
+      <CopyTextButton
+        :text="store.accountFromPrivateKey(privateKey).address"
+        class="opacity-0 group-hover:opacity-100"
+      />
+
+      <TrashIcon
+        v-if="canDelete"
+        class="h-4 w-4 shrink-0 text-gray-400 opacity-0 transition-all hover:text-red-500 group-hover:opacity-100"
+        @click.stop="deleteModalOpen = true"
+      />
+    </button>
+
+    <ConfirmationModal
+      :open="deleteModalOpen"
+      @close="deleteModalOpen = false"
+      @confirm="deleteAddress"
+      buttonText="Delete account"
+      dangerous
+    >
+      <template #title>Delete Account</template>
+      <template #description
+        >Are you sure you want to delete this account?</template
+      >
+      <template #info>
+        <div class="font-mono text-xs">
+          {{ store.accountFromPrivateKey(privateKey).address }}
+        </div>
+      </template>
+    </ConfirmationModal>
+  </div>
 </template>
