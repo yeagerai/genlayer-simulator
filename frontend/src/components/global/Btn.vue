e6162c8338a57f7df046a8b4f62278c1edd301a9
Den <den@Deniss-MacBook-Pro.local>
2024-11-20 12:35:13 +0700

    add call button loading state

--- frontend/src/components/global/Btn.vue
+++ frontend/src/components/global/Btn.vue
@@ -29,7 +29,7 @@ const primary = computed(() => !props.secondary && !props.dangerous);
 
 <template>
   <button
-    class="flex flex-row items-center justify-center gap-2 font-semibold transition-all active:scale-95"
+    class="flex flex-row items-center justify-center font-semibold transition-all active:scale-95"
     :class="[
       primary &&
         'bg-primary text-white hover:bg-opacity-90 hover:text-white dark:bg-accent',
@@ -37,14 +37,19 @@ const primary = computed(() => !props.secondary && !props.dangerous);
         'dark:hover:bg-white-100 border border-gray-300 bg-gray-100 text-gray-800 hover:bg-white hover:text-gray-800 dark:border-none dark:border-gray-400 dark:bg-transparent dark:bg-white dark:bg-opacity-10 dark:text-white dark:shadow-none dark:hover:bg-opacity-20 dark:hover:text-white',
       dangerous && 'bg-red-600 text-white hover:bg-red-500 hover:text-white',
       disabled && 'cursor-not-allowed opacity-50 active:scale-100',
-      tiny && 'rounded px-2 py-1 text-xs',
-      !tiny && 'rounded-md px-3 py-2',
+      tiny && 'gap-1 rounded px-2 py-1 text-xs',
+      !tiny && 'gap-2 rounded-md px-3 py-2',
     ]"
     :disabled="disabled || loading"
     @click="emit('click')"
     :data-testid="testId"
   >
-    <Loader v-if="loading" forceLightColor class="shrink-0" />
+    <Loader
+      v-if="loading"
+      forceLightColor
+      class="shrink-0"
+      :size="tiny ? 14 : 18"
+    />
 
     <component
       v-if="!!icon && !loading"
