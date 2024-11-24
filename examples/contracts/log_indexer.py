f5947168592082515584efc06ba69ee4ae1d68d6
Edgars <edgars.nemse@gmail.com>
2024-11-24 22:05:13 +0000

    fix: improve examples

--- examples/contracts/log_indexer.py
+++ examples/contracts/log_indexer.py
@@ -25,17 +25,17 @@ class LogIndexer:
     def __init__(self):
         pass
 
-    def get_emb_generator(self):
+    def get_embedding_generator(self):
         return genlayermodelwrappers.SentenceTransformer("all-MiniLM-L6-v2")
 
-    def get_emb(
+    def get_embedding(
         self, txt: str
     ) -> np.ndarray[tuple[typing.Literal[384]], np.dtypes.Float32DType]:
-        return self.get_emb_generator()(txt)
+        return self.get_embedding_generator()(txt)
 
     @gl.public.view
     def get_closest_vector(self, text: str) -> dict | None:
-        emb = self.get_emb(text)
+        emb = self.get_embedding(text)
         result = list(self.vector_store.knn(emb, 1))
         if len(result) == 0:
             return None
@@ -49,12 +49,12 @@ class LogIndexer:
 
     @gl.public.write
     def add_log(self, log: str, log_id: int) -> None:
-        emb = self.get_emb(log)
+        emb = self.get_embedding(log)
         self.vector_store.insert(emb, StoreValue(text=log, log_id=u256(log_id)))
 
     @gl.public.write
     def update_log(self, log_id: int, log: str) -> None:
-        emb = self.get_emb(log)
+        emb = self.get_embedding(log)
         for elem in self.vector_store.knn(emb, 2):
             if elem.value.text == log:
                 elem.value.log_id = u256(log_id)
