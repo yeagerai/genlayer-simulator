b6a650dea72160c52c773a6fd1b7738f2b0c6ff5
Den <den@Deniss-MacBook-Pro.local>
2024-11-28 20:56:24 +0700

    fix cleanup local tx logic

--- frontend/src/hooks/useTransactionListener.ts
+++ frontend/src/hooks/useTransactionListener.ts
@@ -16,15 +16,15 @@ export function useTransactionListener() {
   async function handleTransactionStatusUpdate(eventData: any) {
     const newTx = await transactionsStore.getTransaction(eventData.data.hash);
 
-    if (!newTx) {
-      console.warn('Server tx not found for local tx:', newTx);
-      transactionsStore.removeTransaction(newTx);
-      return;
-    }
-
     const currentTx = transactionsStore.transactions.find(
       (t: TransactionItem) => t.hash === eventData.data.hash,
     );
+
+    if (currentTx && !newTx) {
+      // We're cleaning up local txs that don't exist on the server anymore
+      transactionsStore.removeTransaction(currentTx);
+      return;
+    }
 
     if (!currentTx) {
       // This happens regularly when local transactions get cleared (e.g. user clears all txs or deploys new contract instance)
