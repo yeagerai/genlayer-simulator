2db4f4ef23e522b76dd9ea36f1f81373b747f97d
Den <den@Deniss-MacBook-Pro.local>
2024-12-05 14:51:42 +0700

    git tag

--- frontend/src/hooks/useConfig.ts
+++ frontend/src/hooks/useConfig.ts
@@ -1,5 +1,5 @@
 export const useConfig = () => {
-  const studioVersion = import.meta.env.VITE_APP_VERSION;
+  const studioVersion = import.meta.env.VITE_APP_VERSION || 'unknown';
   const isHostedEnvironment = import.meta.env.VITE_IS_HOSTED === 'true';
   const canUpdateValidators = !isHostedEnvironment;
   const canUpdateProviders = !isHostedEnvironment;
